cb7065d7b3b66dc268f8baa0c562ea00e8a96c27
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-23 19:18:14 +1200

    Create route for ListTaskgroup component

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 // ./routes.js
 import Login from "./components/login/Login.vue";
+import ListTaskgroup from "./components/taskgroup/list-taskgroup/ListTaskgroup.vue";
 export const routes = [
   {
     name: "login",
     path: "/login",
     component: Login
+  },
+  {
+    name: "listTaskgroup",
+    path: "/taskgroup/list",
+    component: ListTaskgroup
   }
 ];
