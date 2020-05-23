bed9e10de16ab7c3e06292fdc1e55a09fda8fad9
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-23 13:55:06 +1200

    Update Login component HTML

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -2,6 +2,15 @@
 <template>
   <div>
     <h1>Login</h1>
+    <div>
+      <label for="username">Username:</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" autocomplete="new-password" />
+    </div>
+    <button>Login</button>
   </div>
 </template>
 
