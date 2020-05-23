6cc9cfb55bc5d1a9831e9877d08bfabd225e5d8f
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-23 18:28:45 +1200

    using vue router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
 import axios from "axios";
 
-Vue.prototype.$http = axios;
+import { routes } from "./routes";
 
+Vue.prototype.$http = axios;
+Vue.use(VueRouter);
+
+const router = new VueRouter({ routes });
 new Vue({
   el: "#app",
+  router,
   render: h => h(App)
 });
