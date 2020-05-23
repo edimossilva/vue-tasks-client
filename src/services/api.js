e8b89397caaccc30dfd3939a1b8023f871ba6421
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-23 20:01:58 +1200

    create service to retrieve taskgroups

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,22 @@
 // src/services/api.js
 import axios from "axios";
 
-const HOST = "https://edimossilva-task-manager.herokuapp.com/auth/login";
+const HOST = "https://edimossilva-task-manager.herokuapp.com";
+const LOGIN_URL = `${HOST}/auth/login`;
+const TASKGROUP_URL = `${HOST}/task_lists`;
 
 export const doLoginApi = (username, password) => {
   const params = { username, password };
 
-  return axios.post(HOST, params);
+  return axios.post(LOGIN_URL, params);
+};
+
+export const getTaskgroupsApi = () => {
+  const token = localStorage.getItem("token");
+  const requestConfig = {
+    headers: {
+      Authorization: `Basic ${token}`
+    }
+  };
+  return axios.get(TASKGROUP_URL, requestConfig);
 };
