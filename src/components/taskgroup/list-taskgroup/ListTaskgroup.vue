a858136ba2d3cc2a797325eb8689ec23ca9663a2
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-23 20:04:32 +1200

    Display taskgroups

--- src/components/taskgroup/list-taskgroup/ListTaskgroup.vue
+++ src/components/taskgroup/list-taskgroup/ListTaskgroup.vue
@@ -2,9 +2,27 @@
 <template>
   <div>
     <h1>List taskgroup</h1>
+    <ul>
+      <li v-for="taskgroup in taskgroups" :key="taskgroup.id">
+        {{ taskgroup.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import { getTaskgroupsApi } from "./../../../services/api";
+
+export default {
+  data() {
+    return {
+      taskgroups: []
+    };
+  },
+  mounted: function() {
+    getTaskgroupsApi().then(response => {
+      this.taskgroups = response.data.data;
+    });
+  }
+};
 </script>
