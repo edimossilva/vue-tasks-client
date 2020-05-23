979b24f75e58e57de16cf3bcfedf26386d05e412
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-23 13:39:07 +1200

    Use login component on App component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <login></login>
   </div>
 </template>
 
 <script>
+import Login from "./components/login/Login";
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  components: {
+    login: Login
+  },
+  data() {
     return {
-      msg: 'Welcome :)'
-    }
+      msg: "Welcome :)"
+    };
   }
-}
+};
 </script>
 
 <style>
