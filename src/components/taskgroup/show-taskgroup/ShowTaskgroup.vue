7fecf421bf1fcf7c96f6d40197ae9afe215d7240
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-24 21:14:27 +1200

    Create  ShowTaskgroup component

---
+++ src/components/taskgroup/show-taskgroup/ShowTaskgroup.vue
@@ -0,0 +1,12 @@
+// src/components/taskgroup/show-taskgroup/ShowTaskgroup.vue
+<template>
+  <div>
+    <h1>Show Taskgroup - id: {{id}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"]
+};
+</script>
