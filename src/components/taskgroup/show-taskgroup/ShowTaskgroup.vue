a7567a758383ce418bc5e469ef56569f59374333
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-31 23:58:11 +1200

    adding overview

--- src/components/taskgroup/show-taskgroup/ShowTaskgroup.vue
+++ src/components/taskgroup/show-taskgroup/ShowTaskgroup.vue
@@ -14,6 +14,13 @@
 
     <label for="frequence_type">Frequence Type: {{taskgroup.frequence_type}}</label>
     <br />
+
+    <label>{{taskitemsOverview}}</label>
+    <br />
+
+    <ul>
+      <li v-for="taskitem in formattedTaskitems" :key="taskitem.id">{{taskitem}}</li>
+    </ul>
   </div>
 </template>
 
@@ -30,6 +37,39 @@ export default {
     getTaskgroupApi(this.id).then(response => {
       this.taskgroup = response.data.data;
     });
+  },
+  methods: {
+    formatTaskitem(taskitem) {
+      return {
+        id: taskitem.id,
+        checked: taskitem.checked,
+        name: taskitem.task.name,
+        description: taskitem.task.description
+      };
+    },
+    isTaskitemChecked({ checked }) {
+      return checked;
+    }
+  },
+  computed: {
+    formattedTaskitems: function() {
+      const { taskgroup, formatTaskitem } = this;
+
+      if (!taskgroup.task_in_lists) {
+        return [];
+      }
+
+      const { task_in_lists: taskitems } = taskgroup;
+      return taskitems.map(formatTaskitem);
+    },
+    taskitemsOverview: function() {
+      const { formattedTaskitems, isTaskitemChecked } = this;
+
+      const length = formattedTaskitems.length;
+      const checkedLength = formattedTaskitems.filter(isTaskitemChecked).length;
+
+      return `${checkedLength} of ${length} are checked`;
+    }
   }
 };
 </script>
